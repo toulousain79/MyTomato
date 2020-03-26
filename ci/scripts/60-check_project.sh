619c0bbf5b5c6ba31456ed42076efec7f363ac2e
toulousain79 <toulousain79@users.noreply.github.com>
2020-03-26 09:08:17 +0100

    fix

--- ci/scripts/60-check_project.sh
+++ ci/scripts/60-check_project.sh
@@ -20,34 +20,58 @@ sFilesListTmpl="$(find "${sDirToScan}"/root/TEMPLATEs/ -type f -name "*.tmpl" -p
 if [ -n "${sFilesListTmpl}" ]; then
     echo && echo -e "${CBLUE}*** Check for unused templates ***${CEND}"
     for sFile in ${sFilesListTmpl}; do
-        if (! grep -qR --exclude-dir=.git "${sFile}" "${sDirToScan}"/); then
-            echo -e "${CYELLOW}${sDirToScan}/${sFile}:${CEND} ${CRED}Failed${CEND}"
-            nReturn=$((nReturn + 1))
-        else
-            echo -e "${CYELLOW}${sFile}:${CEND} ${CGREEN}Passed${CEND}"
-        fi
+        case "${sFile}" in
+            *fake-hwclock* | *rpcbind* | *samba* | *openvpn-client* | *openvpn-server* | *ntpd* | *rstats* | *syslog* | *dnsmasq* | *cstats* | *ip_blacklist* | *domains-* | *-rules*)
+                continue
+                ;;
+            *)
+                if (! grep -qR --exclude-dir=.git "${sFile}" "${sDirToScan}"/); then
+                    echo -e "${CYELLOW}${sDirToScan}/${sFile}:${CEND} ${CRED}Failed${CEND}"
+                    nReturn=$((nReturn + 1))
+                else
+                    echo -e "${CYELLOW}${sFile}:${CEND} ${CGREEN}Passed${CEND}"
+                fi
+                ;;
+        esac
     done
 fi
 
 # Templates files called
-sLine="$(grep -rh --exclude-dir=ci --exclude-dir=.git '/TEMPLATEs/' "${sDirToScan}"/)"
+sLine="$(grep -rh --exclude-dir=ci --exclude-dir=.git "TEMPLATEs\|\${gsDirTemplates}" "${sDirToScan}"/ | grep -v 'shellcheck')"
 if [ -n "${sLine}" ]; then
     echo && echo -e "${CBLUE}*** Check for missing templates ***${CEND}"
     for sColumn in ${sLine}; do
         sColumn="$(echo "${sColumn}" | sed "s/\"//g;s/'//g;s/)//g;s/;//g;")"
         if [ -n "${sColumn}" ]; then
-            (grep -q '/TEMPLATEs/' <<<"${sColumn}") && {
-                sTemplate="$(echo "${sColumn}" | cut -d '/' -f 4)"
-                if [ -n "${sTemplate}" ]; then
-                    sFile="$(find "${sDirToScan}"/root/TEMPLATEs/ -type f -name "${sTemplate}")"
-                    if [ -n "${sFile}" ] && [ -f "${sFile}" ]; then
-                        echo -e "${CYELLOW}${sTemplate}:${CEND} ${CGREEN}Passed${CEND}"
-                    else
-                        echo -e "${CYELLOW}${sTemplate}:${CEND} ${CRED}Failed${CEND}"
-                        nReturn=$((nReturn + 1))
+            if (grep -q '.tmpl' <<<"${sColumn}"); then
+                if (grep -q 'TEMPLATEs' <<<"${sColumn}"); then
+                    sTemplate="$(echo "${sColumn}" | cut -d '/' -f 6)"
+                    if [ -n "${sTemplate}" ]; then
+                        sFile="$(find "${sDirToScan}"/root/TEMPLATEs/ -type f -name "${sTemplate}")"
+                        for FILE in ${sFiles}; do
+                            if [ -n "${FILE}" ] && [ -f "${FILE}" ]; then
+                                echo -e "${CYELLOW}${sTemplate}:${CEND} ${CGREEN}Passed${CEND}"
+                            else
+                                echo -e "${CYELLOW}${sTemplate}:${CEND} ${CRED}Failed${CEND}"
+                                nReturn=$((nReturn + 1))
+                            fi
+                        done
+                    fi
+                elif (grep -q "\${gsDirTemplates}" <<<"${sColumn}"); then
+                    sTemplate="$(echo "${sColumn}" | cut -d '/' -f 3)"
+                    if [ -n "${sTemplate}" ]; then
+                        sFiles="$(find "${sDirToScan}"/root/TEMPLATEs/ -type f -name "${sTemplate}")"
+                        for FILE in ${sFiles}; do
+                            if [ -n "${FILE}" ] && [ -f "${FILE}" ]; then
+                                echo -e "${CYELLOW}${sTemplate}:${CEND} ${CGREEN}Passed${CEND}"
+                            else
+                                echo -e "${CYELLOW}${sTemplate}:${CEND} ${CRED}Failed${CEND}"
+                                nReturn=$((nReturn + 1))
+                            fi
+                        done
                     fi
                 fi
-            }
+            fi
         fi
     done
 fi
